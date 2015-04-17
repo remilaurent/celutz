5d20c248bb4903453b9406ecc280dde62869d696
Rémi Laurent <remi.laurent@conostix.com>
2015-04-17 17:52:28 +0200

    better handling of uploaded filenames in the gen tiles tool
    * handling of files with spaces in their name
    * handling of files with multiple dots (.) in their name
    * escaping and quoting as much as possible to avoid issues
    * some basic formatting of log/debug/usage message

--- to_tiles/gen_tiles.sh
+++ to_tiles/gen_tiles.sh
@@ -1,20 +1,9 @@
-#!/bin/bash
-
-trap clean_tmp EXIT
-# fin d'eviter tout problème de locales en reste en C de base.
+#!/bin/sh
 
 set -e
 export LANG=C
 
-# pour éliminer systématiquement les fichier temporaires créés ic
-function clean_tmp() {
-    if [ -n "$wfname" ]; then
-	rm $wfname
-    fi
-    if [ -n "$tmp_file" ]; then
-	rm $tmp_file
-    fi
-}
+trap terminate EXIT
 
 test_mode=false
 memory_limit=256
@@ -22,98 +11,129 @@ crop_x=256
 crop_y=256
 min_scale=0
 max_scale=8
-usage="$0 [-x <x_tile_size>] [-y <y_tile_size>] [-p <prefix_result>] [-t] [-h] [-m <min_zoom>] [-M <max_zoom>] <image_to_convert>\n   example: $0 -r test_res"
+script="$(basename "$0")"
+debug=false
 
-if ! which anytopnm pnmscale convert > /dev/null; then
-    echo "il faut installer les paquets netpbm et imageMagick pour utiliser ce script !"
+log() {
+    echo "$*" >&2
+}
+
+debug() {
+    if [ "true" = "$debug" ]; then
+        echo "$*" >&2
+    fi
+}
+
+terminate() {
+    log "got error"
+    clean_tmp
+}
+
+clean_tmp() {
+    if [ "$wfname" -o "$tmp_file" ]; then
+        rm -f "$wfname" "$tmp_file"
+    fi
+}
+
+usage() {
+    log "$script [OPTION]... <image_to_convert>"
+    log "  -x <x_tile_size>  : size of a tile (default is 256)"
+    log "  -y <y_tile_size>  : size of a tile (default is 256)"
+    log "  -p <prefix_result>: output name prefix, defaut split on '.'"
+    log "  -m <min_zoom>     : min zoom/scale (default is 0)"
+    log "  -M <max_zoom>     : max zoom/scale (default is 8)"
+    log "  -t                : test_mode (default is false)"
+    log "  -h                : this message"
+    log "<image_to_convert>  : input image to convert"
+    log "  example: $basename test_image.jpg"
+}
+
+if ! which anytopnm pnmscale convert bc > /dev/null; then
+    log "il faut installer les paquets"
+    log "  - netpbm"
+    log "  - imagemagick"
+    log "  - bc"
+    log "pour utiliser ce script !"
+    exit 1
 fi
 
-while getopts m:M:x:y:p:ht prs
- do
- case $prs in
-    t)        test_mode=true;;
-    x)        crop_x=$OPTARG;;
-    y)        crop_y=$OPTARG;;
-    m)        min_scale=$OPTARG;;
-    M)        max_scale=$OPTARG;;
-    p)        prefix=$OPTARG;;
-    \? | h)   echo -e $usage
-              exit 2;;
- esac
+while getopts m:M:x:y:p:ht prs; do
+    case $prs in
+        t)        test_mode=true;;
+        x)        crop_x=$OPTARG;;
+        y)        crop_y=$OPTARG;;
+        m)        min_scale=$OPTARG;;
+        M)        max_scale=$OPTARG;;
+        p)        prefix=$OPTARG;;
+        \? | h)   echo -e "$usage"
+                  exit 2;;
+    esac
 done
 shift `expr $OPTIND - 1`
 
-if [ -z "$1" ]; then
-    echo -e "usage :\n$usage"
+fname="$1"
+dir="$(dirname "$fname")"
+
+if [ ! "$fname" ]; then
+    usage
     exit 1
-elif [ ! -f "$1" ]; then
-    echo -e "le paramètre $1 ne correspond pas à un nom de fichier !"
+elif [ ! -r "$fname" ]; then
+    log "can't read input file '$fname'"
     exit 1
 fi
 
-fname=$1
-dir=$(dirname $fname)
-
-if [ -z "$prefix" ]; then
-    prefix=$(basename $1|sed 's/\..*$//')
+if [ ! "$prefix" ]; then
+    # only strip file extension
+    prefix="$(basename "$fname" | sed 's/\.[^\.]*$//')"
 fi
 
-wfname=$(mktemp ${prefix}_XXXX.pnm)
-if ! $test_mode; then
-    anytopnm $fname > $wfname
+wfname="$(mktemp "${prefix}_XXXX.pnm")"
+if [ "false" = "$test_mode" ]; then
+    anytopnm "$fname" > "$wfname"
 else
-    echo "anytopnm $fname > $wfname"
+    log "anytopnm $fname > $wfname"
 fi
 
-echo "préfixe : "$prefix
+tmp_file="$(mktemp)"
 
-tmp_file=$(mktemp)
+for z in $(seq "$min_scale" "$max_scale"); do
+    fprefix="${prefix}_00$z"
+    ratio="$(echo "scale=4; 1 / (2^$z)" | bc -l)"
+    log "generating ratio $ratio"
+    zwfname="$tmp_file"
 
-for ((z=$min_scale; z <= $max_scale; z++))
-do
-    fprefix=${prefix}_00$z
-    printf -v ratio %1.4lf $(echo "1 / (2^$z)" | bc -l)
-    echo génération du ratio $ratio
-    zwfname=$tmp_file
-
-    if $test_mode; then
-	if [ $ratio = 1.0000 ]; then
-	    zwfname=$wfname
-	else
-	    echo "pnmscale $ratio $wfname > $zwfname"
-	fi
-	echo convert $zwfname \
-	    -limit memory $memory_limit \
-            -crop ${crop_x}x${crop_x} \
-            -set filename:tile "%[fx:page.x/${crop_x}]_%[fx:page.y/${crop_y}]" \
-            +repage +adjoin "${fprefix}_%[filename:tile].jpg"
+    if [ "$z" = "0" ]; then
+        zwfname="$wfname"
     else
-	if [ $ratio = 1.0000 ]; then
-	    zwfname=$wfname
-	else
-	    if ! pnmscale $ratio $wfname > $zwfname; then
-		echo "operation 'pnmscale $ratio $wfname > $zwfname' en erreur"
-		exit 1
-	    fi
-	fi
-	if convert $zwfname \
-	    -limit memory $memory_limit \
-            -crop ${crop_x}x${crop_x} \
-            -set filename:tile "%[fx:page.x/${crop_x}]_%[fx:page.y/${crop_y}]" \
-            +repage +adjoin "${fprefix}_%[filename:tile].jpg"; then
-	    echo "Nombre des fichiers produits :" $(ls -la ${fprefix}_*| wc -l)
-	else
-	    echo "operation 'convert' en erreur"
-	    exit 2
-	fi
+        debug "pnmscale '$ratio' '$wfname' > '$zwfname'"
+        if ! pnmscale "$ratio" "$wfname" > "$zwfname"; then
+            log "pnmscale 'pnmscale $ratio $wfname > $zwfname' failed"
+            exit 1
+        fi
+    fi
+    debug "convert '$zwfname' " \
+        "-limit memory '$memory_limit'" \
+        "-crop '${crop_x}x${crop_x}'" \
+        "-set filename:tile '%[fx:page.x/${crop_x}]_%[fx:page.y/${crop_y}]'" \
+        "+repage +adjoin '${fprefix}_%[filename:tile].jpg'"
+    if convert "$zwfname" \
+        -limit memory "$memory_limit" \
+        -crop "${crop_x}x${crop_x}" \
+        -set filename:tile "%[fx:page.x/${crop_x}]_%[fx:page.y/${crop_y}]" \
+        +repage +adjoin "${fprefix}_%[filename:tile].jpg"; then
+        log "generated $(ls "${fprefix}_"*| wc -l) files"
+    else
+        log "convert failed"
+        exit 2
     fi
 done
 
 if ! $test_mode; then
-## les lignes ci dessous sont destinnées à mettre des 0 en debut des numéros de ligne et de colonnes
-## Il y a certainement plus simple mais là c'est du rapide et efficace.
     rename 's/_(\d\d)_(\d+\.jpg)$/_0$1_$2/' ${prefix}_*
     rename 's/_(\d)_(\d+\.jpg)$/_00$1_$2/' ${prefix}_*
     rename 's/_(\d+)_(\d\d)(\.jpg)$/_$1_0$2$3/' ${prefix}_*
     rename 's/_(\d+)_(\d)(\.jpg)$/_$1_00$2$3/' ${prefix}_*
 fi
+
+trap '' EXIT
+clean_tmp
